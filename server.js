3e1089fd80ce9696a73f65360aa4e1addf1533cf
Danil <danil@mail.com>
2024-11-07 21:00:56 +0200

    New files

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const {Pool} = require('pg');
+
+const app = express();
+
+const pool = new Pool({
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
+
+app.get('/', (res, req) => {
+    try {
+        res.status(200).json({ message: 'Success'});
+    } catch(err) {
+        console.error('Error acured');
+        res.status(500).send('Error acured', err);
+    }
+})
+
+app.listen(3000, () => {
+    console.log('The server is running at 3000 port');
+})
